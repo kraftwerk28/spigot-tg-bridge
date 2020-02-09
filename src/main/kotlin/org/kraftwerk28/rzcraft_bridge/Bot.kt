1b645fb76c9d4c5c70d33e6060ae0e61fa384fe9
kraftwerk28 <kefirchik3@gmail.com>
2020-02-09 19:18:51 +0200

    release: version 0.0.2 add: more config options chore: bug fixes

--- src/main/kotlin/org/kraftwerk28/rzcraft_bridge/Bot.kt
+++ src/main/kotlin/org/kraftwerk28/rzcraft_bridge/Bot.kt
@@ -1,70 +1,89 @@
 package org.kraftwerk28.rzcraft_bridge
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.api.methods.ParseMode
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.api.objects.User
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 
 class Bot(plugin: Plugin) : TelegramLongPollingBot() {
     private var plugin: Plugin
     private var allowedChats: List<Long> = listOf()
+    private var chatToMC: Boolean = false
     private var botToken: String = ""
     private var botUsername: String = ""
 
     init {
         this.plugin = plugin
+        chatToMC = plugin.config.getBoolean("logFromTGtoMC", false)
         allowedChats = plugin.config.getLongList("chats")
         botToken = plugin.config.getString("botToken")!!
         botUsername = plugin.config.getString("botUsername")!!
     }
 
     override fun getBotToken() = botToken
+
     override fun getBotUsername() = botUsername
+
     override fun onUpdateReceived(update: Update?) {
         val msg = update?.message
         if (msg == null || msg.text == null) return
         val allowedChats = plugin.config.getLongList("chats")
         if (!allowedChats.contains(msg.chatId)) return
 
-        // /online cmd: shows online players
+        // cmd shows online players
         if (msg.text.startsWith("/online")) {
-            val playerList = plugin.server
-                .getOnlinePlayers()
-                .mapIndexed { i, s -> "${i + 1}. ${s.displayName}" }
-                .joinToString("\n")
+            val playerCount = plugin.server.onlinePlayers.size
+//            val playerList = plugin.server
+//                .getOnlinePlayers()
+//                .mapIndexed { i, s -> "${i + 1}. ${s.displayName}" }
+//                .joinToString("\n")
+            val onlineStr = plugin.config.getString("strings.online", "Online")
+            val offlineStr = plugin.config.getString(
+                "strings.nobodyOnline",
+                "Nobody online"
+            )
             val text =
-                if (playerList.length > 0) ("<b>Online:</b>\n" + playerList)
-                else "<b>Nobody online...</b>"
-            execute(SendMessage(msg.chatId, text).setParseMode("HTML"))
+                if (playerCount > 0) "$onlineStr: $playerCount"
+                else offlineStr
+            execute(SendMessage(msg.chatId, text).setParseMode(ParseMode.HTML))
         }
-
+        // stop, if no command matched:
         if (msg.text!!.startsWith("/")) return
-        plugin.logger.info("Got text: ${msg.text}")
-        plugin.server.broadcastMessage(
-            mcMessageStr(rawUserMention(msg.from), msg.text)
-        )
+
+        if (chatToMC)
+            plugin.sendMessageToMCFrom(rawUserMention(msg.from), msg.text)
     }
 
     fun sendMessageToTGFrom(username: String, text: String) {
         allowedChats.forEach {
-            execute(SendMessage(it, mcMessageStr(username, text)))
+            try {
+                val msg = SendMessage(it, mcMessageStr(username, text))
+                    .setParseMode(ParseMode.HTML)
+                execute(msg)
+            } catch (e: TelegramApiException) {
+            }
         }
     }
 
     fun broadcastToTG(text: String) {
         allowedChats.forEach {
-            execute(SendMessage(it, text))
+            try {
+                val msg = SendMessage(it, text).setParseMode(ParseMode.HTML)
+                execute(msg)
+            } catch (e: TelegramApiException) {
+            }
         }
     }
 
-    private fun commandStr(command: String): String =
-        "/$command@${botUsername}"
-
     private fun mcMessageStr(username: String, text: String): String =
-        "<$username> $text"
+        "<i>$username</i>: $text"
 
     private fun rawUserMention(user: User): String =
-        user.userName ?: user.firstName ?: user.lastName
+        (if (user.firstName.length < 2) null else user.firstName)
+            ?: user.userName
+            ?: user.lastName
 
     private fun telegramUserMention(user: User): String =
         if (user.userName != null) "@${user.userName}"
--- src/main/kotlin/org/kraftwerk28/rzcraft_bridge/Plugin.kt
+++ src/main/kotlin/org/kraftwerk28/rzcraft_bridge/Plugin.kt
@@ -1,12 +1,21 @@
 package org.kraftwerk28.rzcraft_bridge
 
+import com.vdurmont.emoji.EmojiParser
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerChatEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import java.io.File
 
-class Plugin : JavaPlugin() {
-    var tgBot: Bot? = null
+class Plugin : JavaPlugin(), Listener {
+
+    private var tgBot: Bot? = null
+    private var chatToTG: Boolean = false
+
     override fun onEnable() {
         if (!File("plugins/${name}/config.yml").exists()) {
             logger.warning("No config file found! Saving default one.")
@@ -17,10 +26,13 @@ class Plugin : JavaPlugin() {
         ApiContextInitializer.init()
         val botsApi = TelegramBotsApi()
         tgBot = Bot(this)
+        chatToTG = config.getBoolean("logFromMCtoTG", false)
+
         botsApi.registerBot(tgBot)
 
-        server.pluginManager.registerEvents(MessageListener(this), this)
+        server.pluginManager.registerEvents(this, this)
 
+        // Notify everything about server start
         val startMsg = config.getString("serverStartMessage", null)
         if (startMsg != null) tgBot?.broadcastToTG(startMsg)
         logger.info("Plugin started")
@@ -30,5 +42,37 @@ class Plugin : JavaPlugin() {
         val stopMsg = config.getString("serverStopMessage", null)
         if (stopMsg != null) tgBot?.broadcastToTG(stopMsg)
         logger.info("Plugin stopped")
+    }
+
+    fun sendMessageToMC(text: String) {
+        val prep = EmojiParser.parseToAliases(text)
+        server.broadcastMessage(prep)
+    }
+
+    fun sendMessageToMCFrom(username: String, text: String) {
+        val prep = EmojiParser.parseToAliases("<$username> $text")
+        server.broadcastMessage(prep)
+    }
+
+    @EventHandler
+    fun onPlayerChat(event: AsyncPlayerChatEvent) {
+        if (chatToTG)
+            tgBot?.sendMessageToTGFrom(event.player.displayName, event.message)
+    }
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        if (config.getBoolean("logJoinLeave", false)) {
+            val joinStr = config.getString("strings.joined", "joined")
+            tgBot?.broadcastToTG("${event.player.displayName} $joinStr.")
+        }
+    }
+
+    @EventHandler
+    fun onPlayerLeave(event: PlayerQuitEvent) {
+        if (config.getBoolean("logJoinLeave", false)) {
+            val leftStr = config.getString("strings.left", "joined")
+            tgBot?.broadcastToTG("${event.player.displayName} $leftStr.")
+        }
     }
 }
